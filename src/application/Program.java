1e2060d4acaf24f6e41f4c42a5641ecb6061fd56
Pablo Tokuo <pbo_1996@hotmail.com>
2022-11-30 15:22:10 -0300

    SelleDaoJDBC and DaoFactory created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,7 +16,9 @@ public class Program {
         Department d1 = new Department(1, "Books");
         Seller s1 = new Seller(1, "Pablo", "pablo@gmail.com",new Date(), 3000.0, d1);
 
-        System.out.println(s1);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println(sellerDao);
 
     }
 }
